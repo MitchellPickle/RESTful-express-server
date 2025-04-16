91ffd0e08818dc6ccbf177b029e29b22ec9f7073
Mitchell McGaha <mitchell.mcgaha02@gmail.com>
2025-04-15 21:52:10 -0500

    Finished Assignment

--- routes/products.js
+++ routes/products.js
@@ -18,4 +18,76 @@ const validateProduct = (req, res, next) => {
     if(!category || category.trim() === '') {
         errors.push('Product category is required');
     }
+
+    if(errors.length > 0) {
+        return res.status(400).json({ errors });
+    }
+
+    next();
 }
+
+// Get all products
+router.get('/', async (req, res) => {
+    try {
+        const products = await productModel.getAllProducts();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to retrieve products'});
+    }
+});
+
+//Get a product by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await productModel.getProductById(req.params.id);
+        if(!product) {
+            return res.status(404).json({ error: 'Product not found'});
+        }
+
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to retrieve product'});
+    }
+});
+
+// POST create a new product
+router.post('/', validateProduct, async (req, res) => {
+    try {
+        const newProduct = await productModel.createProduct(req.body);
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to create product' });
+    }
+});
+
+// PUT update a product
+router.put('/:id', validateProduct, async (req, res) => {
+    try {
+        const updatedProduct = await productModel.updateProduct(req.params.id, req.body);
+
+        if(!updatedProduct) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to update product' });
+    }
+});
+
+// DELETE a product
+router.delete('/:id', async (req, res) => {
+    try {
+        const success = await productModel.deleteProduct(req.params.id);
+
+        if(!success) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+
+        res.status(204).end();
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to delete product' });
+    }
+});
+
+module.exports = router;
